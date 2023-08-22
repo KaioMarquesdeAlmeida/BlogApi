fc1e28d29e4d3b5aa4ad0071d3b70d81e6470e92
Kaio Marques de Almeida <kaoi.marquesdealmeida@gmail.com>
2023-08-22 16:56:21 -0400

    aasdxassa
    asdxasdsa

--- src/connection.ts
+++ src/connection.ts
@@ -1,5 +1,5 @@
 import { Client } from 'pg';
-const connectionString = 'postgres://kaio:VKaGtjGJL4FR2Gv55OhGd0DKYvxEEC0O@dpg-cjihps8cfp5c7388vqvg-a.ohio-postgres.render.com/kaiolindo';
+const connectionString = 'postgres://kaio:VKaGtjGJL4FR2Gv55OhGd0DKYvxEEC0O@dpg-cjihps8cfp5c7388vqvg-a/kaiolindo';
 const client = new Client(connectionString);
 client.connect();
 
